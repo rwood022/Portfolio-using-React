066e3c22224a3eaf186cd9101c3a100f85d3dc76
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-02-27 16:55:54 -0500

    added bootstrap

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,22 +1,24 @@
-
-//portfolio page 6 apps with images that link to deployed sites
-
-
-//resume - link to dowloadable resume and list of proficiencies
 import React from 'react';
 // Here we import the Section.css file to grant access to some additional classNames
-import '../styles/Project.css';
+// import '../styles/Project.css';
 
 // TODO: Create a styles object called "styles"
 
 function Project() {
   // TODO: Add a style attribute to `section`
-  <section id="portfolio">
-    <h1>Here's Some of My Work!</h1>
-    <div id="portfolio-layout">
-      {/* <img src={`${item.imgurl}`} className="project1"></img> */}
-    </div>
-  </section>
+  const message = "Here's Some of My Work!";
+  
+  return (
+  <div id="portfolio">
+    <h1>{message}</h1>
+  </div>
+  )
 }
 
 export default Project;
+
+
+//portfolio page 6 apps with images that link to deployed sites
+
+
+//resume - link to dowloadable resume and list of proficiencies
