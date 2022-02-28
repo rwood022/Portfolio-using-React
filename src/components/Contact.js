b12541ac923f9667e092c8e8370e76a92f912f3c
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-02-28 16:33:10 -0500

    added contact box

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
+
+const styles = {
+    contactStyle: {
+      color: "white",
+      justifyContent: 'center',
+      background: "#e5989b",
+    },
+};
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function validateMessage(message) {
+    const re = /^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\X])(?=.*[!$#%]).*$/;
+}
 
 export default function Contact() {
     // card for name, email and description
-    const styles = {
-        contactStyle: {
-          color: "white",
-          justifyContent: 'center',
-          background: "#e5989b",
-        },
-      };
+    const [email, setEmail] = useState('');
+    const [userName, setUserName] = useState('');
+    const [message, setMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+
+    const handleInputChange = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        if (inputType === 'email') {
+            setEmail(inputValue);
+        } else if (inputType === 'userName') {
+            setUserName(inputValue);
+        } else {
+            setMessage(inputValue);
+        }
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if (!validateEmail(email)) {
+            setErrorMessage('Email is invalid');
+            return;
+        }
+        if (!validateMessage(message)) {
+            setErrorMessage('Message');
+            return;
+        }
+
+        alert(`Thank you, ${userName}. Your message and contact has been sent!`);
+
+        setUserName('');
+        setEmail('');
+        setMessage('');
+
+    }
+
+
     return(
         <div style={styles.contactStyle} className="contact">
             <h2>Please contact me for more info!</h2>
+            <form className="form">
+                <input
+                value={userName}
+                name="userName"
+                onChange={handleInputChange}
+                type="text"
+                placeHolder="username"
+                />
+                <input
+                value={email}
+                 name="email"
+                onChange={handleInputChange}
+                type="email"
+                placeholder="email"
+                />
+                <input
+                value={message}
+                name="message"
+                onChange={handleInputChange}
+                type="text"
+                placeholder="message"
+                />
+                <button type="button" onClick={handleFormSubmit}>Submit</button>
+            </form>
+            {errorMessage && (
+            <div>
+                <p className="error-text">{errorMessage}</p>
+             </div>
+             )}
         </div>
     );
 }
